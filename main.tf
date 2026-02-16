228ac4159dc89a12f88efd3dca2b300d7dc7a096
chamu <chamukallutla@gmail.com>
2026-02-16 19:38:46 +0530

    rds create

--- main.tf
+++ main.tf
@@ -179,9 +179,6 @@ resource "aws_ecs_service" "fargate_service" {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
 
 resource "aws_db_subnet_group" "mysql_subnet_group" {
   name       = "mysql-subnet-group"
