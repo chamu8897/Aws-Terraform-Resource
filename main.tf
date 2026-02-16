563c9b2ed9599639d4125a5ff0ca668ec7cb20e3
chamu <chamukallutla@gmail.com>
2026-02-16 20:34:47 +0530

    ec2 create

--- main.tf
+++ main.tf
@@ -206,10 +206,6 @@ resource "aws_db_instance" "mysql" {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
